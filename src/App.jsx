1ff8efabbd31e200b5d248a94b7fd5f9c0531429
Yathish Acharya <yathishkumar2013@gmail.com>
2024-11-24 12:45:53 +0530

    converter app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl bg-black text-white font-semibold p-6">Converter</div>
+    </>
+  )
+}
+
+export default App
